09d86e4de665ced24a68b6ad832d4a9767fd9ab5
Rajaneesh2307 <yembulururajaneesh@gmail.com>
2025-11-05 16:07:25 +0530

    Added New Ifo

--- StudentInfo.java
+++ StudentInfo.java
@@ -22,7 +22,7 @@ public class StudentInfo {
         System.out.println("  6. Build Automation Tool");
         System.out.println("  8. Artifactory Storage Tool");
         System.out.println("  9. Continues Code Quality Check and Security Analysis Tool");
-	System.out.println(" 10. CI/CD Automation Tool");
+	System.out.println(" 10. CI/CD Automation Tool we have two options one is Jenkins and another one is AWSCodePipeline");
         System.out.println("--------------------------------------");
         System.out.println("Total Credits : 60");
         System.out.println("Semester      : 8th");
